c09962c9cb00390900f22d7acb346e840a980ef1
adi <adipeled@gmail.com>
2020-12-20 10:22:07 +0200

    2th

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -6,23 +6,11 @@ export const loadContacts = (filter) => async dispatch => {
     return dispatch({ type: 'SET_CONTACTS', contacts })
 }
 
-// save or update worker, then 
-// getting workers from backend and sending to reducer to update state
-// export const addUpdateWorker = (worker) => async dispatch => {
-//     await workerService.saveWorker(worker);
-//     const response = await workerService.getWorkers()
-//     return dispatch({ type: 'SET_WORKERS', payload: response })
-// }
-
-// delete worker, then
-// getting workers from backend and sending to reducer to update state
-
 export const removeContact = (id) => async dispatch => {
     await contactService.removeContact(id);
     const contacts = await contactService.getContacts()
     return dispatch({ type: 'SET_CONTACTS', contacts })
 }
-
 
 export const addContact = (contact) => async dispatch => {
     await contactService.addContact(contact)
